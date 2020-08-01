099fc5bcb4f48573b376965cecc075e9d7384caa
sharontlin <sharonlin14@gmail.com>
2020-08-01 15:02:20 -0400

    Initial commit

---
+++ src/api/quizQuestions.js
@@ -0,0 +1,89 @@
+var quizQuestions = [
+  {
+      question: "What are your daily activities like?",
+      answers: [
+          {
+              type: "High",
+              content: "Hanging out with my friends in the city!"
+          },
+          {
+              type: "Medium",
+              content: "Taking daily walks and eating out at restaurants every now and then."
+          },
+          {
+              type: "Low",
+              content: "I'm usually chilling at home."
+          }
+      ]
+  },
+  {
+      question: "How often do you wear your mask?",
+      answers: [
+          {
+              type: "High",
+              content: "I don't need to wear a mask, it's not very crowded here."
+          },
+          {
+              type: "Medium",
+              content: "Whenever I'm in a crowded area."
+          },
+          {
+              type: "Low",
+              content: "Everywhere I go."
+          }
+      ]
+  },
+  {
+      question: "What kind of mask do you wear?",
+      answers: [
+          {
+              type: "High",
+              content: "What kind of mask do you wear?"
+          },
+          {
+              type: "Medium",
+              content: "I use a cloth mask."
+          },
+          {
+              type: "Low",
+              content: "I use a face shield and/or N95 mask."
+          }
+      ]
+  },
+  {
+      question: "What's your health like?",
+      answers: [
+          {
+              type: "High",
+              content: "I have some health problems with my lungs/heart/immune system."
+          },
+          {
+              type: "Medium",
+              content: "I'm healthy but often around close groups of people."
+          },
+          {
+              type: "Low",
+              content: "I'm pretty healthy and exercise regularly and away from other people."
+          }
+      ]
+  },
+  {
+      question: "Do you know how to get checked for COVID-19?",
+      answers: [
+          {
+              type: "High",
+              content: "No, I have no idea."
+          },
+          {
+              type: "Medium",
+              content: "No, but I do have health insurance."
+          },
+          {
+              type: "Low",
+              content: "Yes, I already know exactly who to call."
+          }
+      ]
+  }
+];
+
+export default quizQuestions;
